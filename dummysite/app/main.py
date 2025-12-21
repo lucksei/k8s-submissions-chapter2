58d971a47f2de0810d52ec218444ecea32a84ba4
lucksei <luca6mandi@gmail.com>
2025-12-21 03:24:36 +0000

    modified the dummysite app to load css, images, etc... (very poorly)

--- dummysite/app/main.py
+++ dummysite/app/main.py
@@ -1,17 +1,35 @@
 import os
+from typing import Dict
+from dataclasses import dataclass
 from starlette.applications import Starlette
 from starlette.responses import HTMLResponse
 from starlette.routing import Route
 from starlette.requests import Request
 import requests
+import urllib
 import uvicorn
-
-PORT = os.environ.get("PORT", "42069")
-WEBSITE_URL = os.environ.get("WEBSITE_URL")
 
 # -----
 # Utils
 # -----
+
+
+@dataclass
+class Config:
+    port: int
+    website_url: str
+
+    @classmethod
+    def from_env(cls):
+        try:
+            port_int = int(os.environ.get("PORT", "42069"))
+        except ValueError:
+            port_int = 42069
+
+        return cls(
+            port=port_int,
+            website_url=os.environ.get("WEBSITE_URL"),
+        )
 
 
 def get_website(website_url: str) -> str:
@@ -22,17 +40,53 @@ def get_website(website_url: str) -> str:
             f"Failed to fetch webpage: {response.status_code}. {response.text}")
     return response.text
 
+
+def get_content(path: str) -> str:
+    """Helper to fetch css"""
+    url = urllib.parse.urljoin(config.website_url, path)
+    allowed_content_types = ["text/css",
+                             "text/css; charset=utf-8", "image/png", "image/gif", "image/jpeg"]
+    response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+    if response.status_code != 200:
+        raise Exception(
+            f"Failed to fetch webpage: {response.status_code}. {response.text}")
+    if response.headers.get("Content-Type").lower() not in allowed_content_types:
+        raise Exception(
+            f"Invalid content type: {response.headers.get('Content-Type')}")
+
+    return response.content
+
 # -----
 # App
 # -----
 
 
 async def index(request: Request) -> HTMLResponse:
-    website_html = request.app.state.website_html
-    return HTMLResponse(content=website_html, status_code=200)
+    if not request.app.state.content.get("_index_"):
+        try:
+            request.app.state.content["_index_"] = get_website(
+                config.website_url)
+        except Exception as e:
+            request.app.state.content["_index_"] = str(
+                f"<html><body><h1>Error</h1><p>Could not fetch website: {e}</p></body></html>"
+            )
+    return HTMLResponse(content=request.app.state.content.get("_index_"), status_code=200)
+
+
+async def content(request: Request) -> HTMLResponse:
+    path = request.url.path
+    if not request.app.state.content.get(path):
+        try:
+            request.app.state.content[path] = get_content(path)
+        except Exception as e:
+            request.app.state.content[path] = str(
+                f"<html><body><h1>Error</h1><p>Could not fetch content: {e}</p></body></html>"
+            )
+    return HTMLResponse(content=request.app.state.content.get(path), status_code=200)
 
 routes = [
-    Route("/", index, methods=["GET"])
+    Route("/", index, methods=["GET"]),
+    Route("/{path:path}", content, methods=["GET"])
 ]
 
 app = Starlette(debug=True, routes=routes)
@@ -42,18 +96,13 @@ app = Starlette(debug=True, routes=routes)
 # -----
 
 if __name__ == "__main__":
-    print("Loading website")
-    try:
-        app.state.website_html = get_website(WEBSITE_URL)
-    except Exception as e:
-        print(f"Failed to load website: {e}")
-        exit(1)
+    print("Loading configuration")
+    config = Config.from_env()
+    app.state.content = {}
 
-    port = 42069
-    try:
-        port = int(PORT)
-    except ValueError:
-        print(f"Failed to parse port: {PORT}. Defaulting to 42069")
+    if not config.website_url:
+        raise Exception("Missing environment variable: WEBSITE_URL")
 
-    print(f"Starting server on port {port}")
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    print(
+        f"Starting server on port {config.port}.\nWebsite URL: {config.website_url}")
+    uvicorn.run(app, host="0.0.0.0", port=config.port)
